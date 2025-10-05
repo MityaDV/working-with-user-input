0e89731c4f64230555d1db1eb3a19e3909046e5c
Dmitry Kovalkov <dmitriy.k@kultprosvet.net>
2025-10-05 20:35:06 +0300

    178. Отправка Формы и Получение Данных Пользователя

--- src/components/SomeInput.js
+++ src/components/SomeInput.js
@@ -1,9 +1,35 @@
+import { useRef, useState } from 'react';
+
 const SomeInput = (props) => {
+  // 2 подход
+  const nameInputRef = useRef();
+
+  // 1 подход
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+
+    console.log(enteredName);
+    console.log(nameInputRef.current.value);
+    setEnteredName('');
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label htmlFor="name">Введите Имя</label>
-        <input type="text" id="name" />
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          value={enteredName}
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Отправить</button>
